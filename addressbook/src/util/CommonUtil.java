b286f8b072ddf8eb26ce9ed0e33d9a78cc96ea65
shinichi <uno.threemouth@gmail.com>
2013-05-05 01:38:05 +0900

    create CommonUtil

---
+++ addressbook/src/util/CommonUtil.java
@@ -0,0 +1,29 @@
+package util;
+
+/**
+ * 共通値保持クラス
+ * @author shin
+ *
+ */
+public class CommonUtil {
+
+	/* ----------- 各処理時に必要な配列数 --------------- */
+
+	/** 登録時 **/
+	public static final int ARRAY_NUM_INSERT = 4;
+
+	/** 更新時 **/
+	public static final int ARRAY_NUM_UPDATA = 4;
+
+	/** 削除時 **/
+	public static final int ARRAY_NUM_DELETE = 2;
+
+	/** 全削除時 **/
+	public static final int ARRAY_NUM_ALL_DELETE = 1;
+
+	/** 検索時 **/
+	public static final int ARRAY_NUM_SEARCH = 2;
+
+	/** 一覧表示時 **/
+	public static final int ARRAY_NUM_SHOW_ALL = 1;
+}
